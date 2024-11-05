67818251fc0d2fe83882a8f3cce2ec902f54dcbe
Alexey Rybakov <rybakov.aax@gmail.com>
2024-11-05 18:30:15 +0300

    person

---
+++ src/person.py
@@ -0,0 +1,42 @@
+# ==================================================================================================
+
+class Person:
+    """
+    Person.
+    """
+
+# --------------------------------------------------------------------------------------------------
+
+    def __init__(self, name, position, table_number):
+        """
+        Constructor.
+
+        Parameters
+        ----------
+        name : str
+            Name of person.
+        position : str
+            Position string.
+        table_number : int
+            Table number.
+        """
+
+        self.name = name
+        self.position = position
+        self.table_number = table_number
+
+# --------------------------------------------------------------------------------------------------
+
+    def __repr__(self):
+        """
+        Convert into string.
+
+        Returns
+        -------
+        str
+            String representation.
+        """
+
+        return f'{self.name} ({self.position}, {self.table_number})'
+
+# ==================================================================================================
